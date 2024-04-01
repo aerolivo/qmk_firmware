0671df2815722a9c8d84f5903b206f142dbaf8a3
aero@lhcpheno32 <andres.renteria@ific.uv.es>
2024-04-01 16:59:45 +0200

    adding 4x12_equals_blok folder and modification and keeping updated with the original master fork

---
+++ keyboards/boardsource/4x12_equals_blok/rules.mk
@@ -0,0 +1,12 @@
+# This file intentionally left blank
+#
+
+# 2024.01.22 : modification by Andres Renteria
+# Enabling OS detection
+OS_DETECTION_ENABLE = yes
+
+# Backlight
+BACKLIGHT_ENABLE = yes
+
+
+
